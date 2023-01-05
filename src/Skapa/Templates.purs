a14a0b323f06d1c781fa433da26c292b30123ac5
Rickard Andersson <gonz@severnatazvezda.com>
2023-01-05 09:35:15 +0200

    refactor: move exported functions up in `Templates`

--- src/Skapa/Templates.purs
+++ src/Skapa/Templates.purs
@@ -1,6 +1,7 @@
 module Skapa.Templates
   ( instantiate
   , pathToTemplate
+  , pathsToTemplate
   ) where
 
 import Prelude
@@ -9,7 +10,7 @@ import Data.Array as Array
 import Data.Foldable (foldMap)
 import Data.Maybe (Maybe(..))
 import Data.TemplateString as TemplateString
-import Data.Traversable (traverse)
+import Data.Traversable (sequence, traverse)
 import Data.Tuple (Tuple)
 import Effect.Aff (Aff)
 import Effect.Class (liftEffect)
@@ -24,6 +25,17 @@ instantiate :: Template -> Array (Tuple String String) -> Array FileOutput
 instantiate (Template { entities }) variables =
   foldMap (instantiateEntity variables) entities
 
+-- | Turns an array of paths into a template, reading all files for all paths.
+pathsToTemplate :: TemplateId -> TemplateDescription -> Array String -> Aff (Maybe (Array Template))
+pathsToTemplate id description paths = do
+  sequence <$> traverse (pathToTemplate id description) paths
+
+-- | Turns a path into a template, reading all files for the path if it's a directory or just the
+-- | file if it's a file.
+pathToTemplate :: TemplateId -> TemplateDescription -> String -> Aff (Maybe Template)
+pathToTemplate id description path =
+  map (\entity -> Template { id, description, entities: [ entity ] }) <$> pathToEntity path
+
 instantiateEntity :: Array (Tuple String String) -> Entity -> Array FileOutput
 instantiateEntity variables (File { path, content }) =
   { path, contents: TemplateString.template content variables } # FileOutput # Array.singleton
@@ -36,17 +48,10 @@ prependPath path (File { path: childPath, content }) =
 prependPath path (Directory { path: childPath, children }) =
   Directory { path: path <> "/" <> childPath, children }
 
-pathToTemplate :: TemplateId -> TemplateDescription -> String -> Aff (Maybe Template)
-pathToTemplate id description path =
-  map (\entity -> Template { id, description, entities: [ entity ] }) <$> pathToEntity path
-
 pathToEntity :: String -> Aff (Maybe Entity)
 pathToEntity path = do
   stats <- FileSystem.stat path
-  if Stats.isDirectory stats then
-    directoryToEntity path
-  else
-    fileToEntity path
+  if Stats.isDirectory stats then directoryToEntity path else fileToEntity path
 
 directoryToEntity :: String -> Aff (Maybe Entity)
 directoryToEntity path = do
